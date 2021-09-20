c463c7ef9fe74554776537bc4bf4fb9ef33bd479
Miguel <miguel14lopez99@gmail.com>
2021-09-20 14:39:55 +0200

    he empezado los ejercicios

---
+++ Resultado1/Resultado1/src/main/java/prueba/File1Mostrando.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+import java.io.File;
+
+/**
+ *
+ * @author chipi
+ */
+public class File1Mostrando {
+    public static void main(String[ ] args){
+        System.out.println("Ficheros en el directorio del proyecto: ") ;
+        File fi=new File(".");
+        String[] archivos = fi.list();
+        for (int i=0;i<archivos.length;i++) {
+            System.out.println(archivos[i]);
+
+        }
+    }
+}
+
--- Resultado1/Resultado1/src/main/java/prueba/File2Creando.java
+++ Resultado1/Resultado1/src/main/java/prueba/File2Creando.java
@@ -3,11 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package prueba;
+import java.io.File;
 
 /**
  *
  * @author chipi
  */
-public class File1Mostrando {
+public class File2Creando {
+    
+    public static void main(String[] args) {
+        File directorio = new File(".\\carpeta");
+        directorio.mkdir();
+        
+    }
+    
     
 }
