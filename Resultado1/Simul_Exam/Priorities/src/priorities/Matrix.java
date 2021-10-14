5c8a43e6f737687a16110af816a718f8978ff743
Miguel <miguel14lopez99@gmail.com>
2021-10-14 14:00:06 +0200

    guardado matrix

---
+++ Resultado1/Simul_Exam/Priorities/src/priorities/Matrix.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package priorities;
+
+import java.util.Queue;
+
+/**
+ *
+ * @author b15-04m
+ */
+public class Matrix {
+    
+    Character[] matrix;
+    Queue<Character> people;
+    SortCharacters priorityQueue;
+    
+    
+    
+}
